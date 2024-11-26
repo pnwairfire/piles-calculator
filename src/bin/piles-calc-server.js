edbe19262dd7aefadca61935633935417ec42335
Joel Dubowy <jdubowy@gmail.com>
2024-11-26 12:36:42 -0800

    Use nodemon for reloading server

--- src/bin/piles-calc-server.js
+++ src/bin/piles-calc-server.js
@@ -4,22 +4,18 @@ import path from 'path'
 import yargs from 'yargs/yargs'
 import { Hono } from 'hono'
 import { serve } from '@hono/node-server'
-import livereload from 'livereload'
 
 import { calcPiles } from '../index'
 
 var argv = yargs(process.argv.slice(2))
-    .parse();
+  .usage('Usage: $0 --port [num]')
+  .help('h').alias('h', 'help')
+  .parse();
 
 const app = new Hono()
 
-app.get('/', (c) => c.json({msg: 'not yet implemented!'}))
-
-if (argv.dev) {
-    const liveReloadServer = livereload.createServer();
-    const watchDir = path.dirname(path.dirname(__filename))
-    console.log(`Watching ${watchDir} for updates to live reload`)
-    liveReloadServer.watch(watchDir);
-}
+app.get('/', (c) => {
+  return c.json({msg: 'not yet implemented!'})
+})
 
 serve(app)
