60a25e63cd3be908f1cb1802516ee037f59d7de0
Joel Dubowy <jdubowy@gmail.com>
2024-11-25 17:14:27 -0800

    Continue putting together project structure

---
+++ tests/index.test.js
@@ -0,0 +1,7 @@
+import { caclPiles } from "../src/index";
+
+test("returns 1", () => {
+  //console.log = jest.fn();
+  const r = calcPiles();
+  //expect(console.log).toHaveBeenCalledWith("Hello, world!");
+});
